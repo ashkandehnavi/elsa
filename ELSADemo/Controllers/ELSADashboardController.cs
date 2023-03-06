d91fbf6db8e68112209ec7c3d5472f5efa80ff03
Ashkan Dehnavi <ashkandehnavi@gmail.com>
2023-03-06 09:00:05 +0330

    init

---
+++ ELSADemo/Controllers/ELSADashboardController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ELSADemo.Controllers;
+public class ELSADashboardController : Controller
+{
+	public IActionResult Index()
+	{
+		return View();
+	}
+}
---
+++ ELSADemo/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ELSADemo.Models;
+
+public class ErrorViewModel
+{
+	public string? RequestId { get; set; }
+
+	public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
